b36be8bcbba94faec94540839fb19bae1f864d99
jaquelinepires <piresjaqueline17@gmail.com>
2022-01-31 21:57:15 +0000

    Component Input

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import {
   Poppins_700Bold,
 } from "@expo-google-fonts/poppins";
 // import { View, Text } from "react-native";
-import { Dashboard } from "./src/screens/Dashboard";
+import { Register } from "./src/screens/Register";
 
 export default function App() {
   //fonts
@@ -23,7 +23,7 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   );
 }
