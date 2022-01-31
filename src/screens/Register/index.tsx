53c32ac8bf6e3a0a03366ddc584bb05315e00eef
jaquelinepires <piresjaqueline17@gmail.com>
2022-01-31 22:25:38 +0000

    Component button

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Button } from "../../components/Form/Button";
 import { Input }  from '../../components/Form/Input'
 import {
    Container,
    Header,
    Title,
-   Form
+   Form,
+   Fields
    } from "./styles";
 
 export function Register() {
@@ -15,11 +17,16 @@ export function Register() {
       </Header>
 
       <Form>
-      <Input 
-        placeholder="Nome"/>
-      <Input 
-        placeholder="Preço"/>
+        <Fields>
+          <Input 
+            placeholder="Nome"/>
+          <Input 
+            placeholder="Preço"/>
+        </Fields>
+        
+        <Button title="Enviar"/>
       </Form>
+
     </Container>
   )
 }
