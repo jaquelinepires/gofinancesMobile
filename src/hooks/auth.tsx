757c058a17727a9756f2126d9be580dc8111f6c7
jaquelinepires <piresjaqueline17@gmail.com>
2022-02-09 22:50:03 -0300

    google account authentication

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, ReactNode, useContext } from 'react';
+import React, { createContext, ReactNode, useContext, useState } from 'react';
+import * as AuthSession from 'expo-auth-session';
 
 interface AuthProviderProps {
   children: ReactNode;
@@ -11,17 +12,53 @@ interface User {
 }
 interface IAuthContextData {
   user: User;
+  signInWithGoogle(): Promise<void>; 
+}
+interface AuthorizationResponse {
+  params: {
+    access_token: string;
+  } 
+  type: string;
 }
 const AuthContext = createContext({} as IAuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
-  const user = { 
-    id: '1213123',
-    name: 'Jaqueline Pires',
-    email: 'jaqueline@email.com'
-  };
+  const [user, setUser ] = useState<User>({} as User);
+
+  //autenticação Google
+  async function signInWithGoogle() {
+    try {
+      const CLIENT_ID = '780919981816-0aqg96f7mv8cmj8at6u1ru2gd3o9tj58.apps.googleusercontent.com';
+      const REDIRECT_URI = 'https://auth.expo.io/@jaquelinepires/gofinances';
+      const RESPONSE_TYPE = 'token';
+      const SCOPE = encodeURI('profile email');
+
+      const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+
+      const { type, params } = await AuthSession
+      .startAsync({ authUrl }) as AuthorizationResponse;
+
+      if(type === 'success'){
+        const response = await fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`);
+        const userInfo = await response.json()
+
+        setUser({
+         id: userInfo.id,
+         email: userInfo.email,
+         name: userInfo.given_name,
+         photo: userInfo.picture
+        });
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
   return(
-    <AuthContext.Provider value={{user}}>
+    <AuthContext.Provider value={{
+      user, 
+      signInWithGoogle
+      }}>
        {children}
     </AuthContext.Provider>
   )
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+import React from "react";
+import { Alert }  from 'react-native'
 import { RFValue } from "react-native-responsive-fontsize";
 import AppleSvg from '../../assets/apple.svg'
 import GoogleSvg from '../../assets/google.svg'
@@ -16,41 +17,54 @@ import {
 } from './styles'
 
 export function SignIn() {
-  const { user } = useAuth();
-  console.log(user.name)
+  const { signInWithGoogle } = useAuth();
 
+  async function handleSignInWithGoogle(){
+    try {
+      await signInWithGoogle()
+
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Não foi possivel concetar a conta Google')
+    }
+  }
 
   return (
     <Container>
       <Header>
         <TitleWrapper>
-          <LogoSvg 
+          <LogoSvg
             width={RFValue(120)}
             height={RFValue(68)}
           />
+          
           <Title>
             Controle suas {'\n'}
-            finanças de forma{'\n'} 
+            finanças de forma {'\n'}
             muito simples
           </Title>
         </TitleWrapper>
+
         <SignInTitle>
           Faça seu login com {'\n'}
           uma das contas abaixo
         </SignInTitle>
       </Header>
+
       <Footer>
         <FooterWrapper>
-        <SignInSocialButton
-          title="Entrar com Google"
-          svg={GoogleSvg}
-        />
-        <SignInSocialButton
-           title="Entrar com Apple"
-           svg={AppleSvg}
-        />
+          <SignInSocialButton
+            title="Entrar com Google"
+            svg={GoogleSvg}
+            onPress={handleSignInWithGoogle}
+          />
+
+          <SignInSocialButton
+            title="Entrar com Apple"
+            svg={AppleSvg}
+          />
         </FooterWrapper>
       </Footer>
     </Container>
-  )
+  );
 }
